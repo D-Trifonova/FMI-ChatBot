af579962525aa8e9455014acc5c504e49e1a7e0c
fmi <fmi@fmi.uni-plovdiv.bg>
2025-01-12 02:57:33 -0800

    change port to 8000

--- chatbot-ui/src/service/chatService.ts
+++ chatbot-ui/src/service/chatService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "http://localhost:8000/api";
 
 export const sendMessageToApi = async (message: string): Promise<string> => {
   try {
